e315c89ec8faa5e7a9a13c7aca26cffec8258c4a
Armine Shahbazyan <ashahbaz@c1r2s3.42yerevan.am>
2025-03-05 18:37:24 +0400

    norm

--- handle_movement.c
+++ handle_movement.c
@@ -1,18 +1,6 @@
 # include "cub3d.h"
 
 
-// static void	clear_current_position(t_game *game)
-// {
-// 	int	x;
-// 	int	y;
-
-// 	x = (int)(game->player.x / SPRITE);
-// 	y = (int)(game->player.y / SPRITE);
-// 	game -> map[y][x] = '0';
-// 	draw_square(game, game->player.x - SPRITE / 2, game->player.y- SPRITE / 2, 15, 16777215);
-// 	draw_direction(game,16777215);
-// }
-
 void draw_direction(t_game *game, int color)
 {
     int line_length = 20;
@@ -38,38 +26,10 @@ static void move_player(t_game *game, double dir_x, double dir_y)
 	    return ;
 	game -> player.x += dir_x * SPRITE;
 	game -> player.y += dir_y * SPRITE;
-	// draw_map(game);
-	// mlx_put_image_to_window(game->mlx, game->mlx_win, game->img.img, 0, 0);
 }
-
-int	handle_movement(int keycode, t_game *game)
+static void rotate_player(int keycode, t_game *game)
 {
-		//if (keycode == EXIT) HANDLE
-	if (keycode == W)
-		move_player(game, game -> player.dx, game -> player.dy);
-	else if (keycode == S)
-		move_player(game, -game -> player.dx, -game -> player.dy);
-	else if (keycode == A)
-	{
-		// if (game -> player.dx == 0)
-		// {
-
-		// 	game -> player.dx++;
-		// }
-		move_player(game, -1, 0);
-	}
-	else if (keycode == D)
-	{
-		// if (game -> player.dx == (double)0)
-		// {
-		// 	game -> player.dx++;
-
-		// }
-		move_player(game, 1, 0);
-	}
-	else if (keycode == L_A || keycode == R_A)
-	{
-		if (keycode == L_A)
+	if (keycode == L_A)
 		{
 			game->player.angle -= 0.1;
 			if (game->player.angle < 0)
@@ -83,9 +43,22 @@ int	handle_movement(int keycode, t_game *game)
 		}
 		game -> player.dx = cos(game -> player.angle);
 		game -> player.dy = sin(game -> player.angle);
-	}
-		mlx_clear_window(game->mlx, game->mlx_win);
-		draw_map(game);
-		mlx_put_image_to_window(game->mlx, game->mlx_win, game->img.img, 0, 0);
+}
+int	handle_movement(int keycode, t_game *game)
+{
+		//if (keycode == EXIT) HANDLE
+	if (keycode == W)
+		move_player(game, game -> player.dx * 0.5, game -> player.dy * 0.5);
+	else if (keycode == S)
+		move_player(game, -game -> player.dx * 0.5, -game -> player.dy * 0.5);
+	else if (keycode == A)
+		move_player(game, -0.5, 0);
+	else if (keycode == D)
+		move_player(game, 0.5, 0);
+	else if (keycode == L_A || keycode == R_A)
+		rotate_player(keycode, game);
+	mlx_clear_window(game->mlx, game->mlx_win);
+	draw_map(game);
+	mlx_put_image_to_window(game->mlx, game->mlx_win, game->img.img, 0, 0);
 	return (0);
 }
